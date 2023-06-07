0c9a51fe8f2e831bbbaf03ee3dcaf3f7dd3f74db
CarolineNyam <caroline.nyamupanda@mycit.ie>
2023-06-07 08:32:22 +0100

    Classes

---
+++ Nyamupanda_Caroline_class..py
@@ -0,0 +1,17 @@
+# Caroline NYAMUPANDA
+# R00206878
+
+from Nyamupanda_Caroline_q4 import Musicians
+
+
+def main():
+    m1 = Musicians("Gwen", "piano", "3", )
+    m2 = Musicians("David", "violin", "1", )
+    print(m1)
+    print(m2)
+    print(m1.level())
+    print(m2.level())
+
+
+if __name__ == '__main__':
+    main()
