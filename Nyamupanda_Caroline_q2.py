483cec287fa95e8a057d588dace5cbe1f4e87f39
CarolineNyam <caroline.nyamupanda@mycit.ie>
2023-06-07 08:34:54 +0100

    Classes

---
+++ Nyamupanda_Caroline_q2.py
@@ -0,0 +1,16 @@
+# Caroline NYAMUPANDA
+# R00206878
+
+day = ["Mon", "Tues", "Wed", "Thurs", "Fri"]
+sales = [105, 44, 22, 121, 14]
+
+
+for i in range(len(day)):
+    print(day[0])
+
+profits = 0
+for sale in sales:
+    profits += len(sales)
+    if profits > 50:
+        print(sales, '*')
+
